14270ae9561da1451d057b8059915002ea3d952c
adrianmudarramartin <adrianmudarramartin@gmail.com>
2022-01-23 12:57:07 +0100

    Versión actualizada a valpha.0.2

--- bot.py
+++ bot.py
@@ -8,22 +8,38 @@ import sqlite3
 # Inicialización de la base de datos
 conexion= sqlite3.connect('Datos.sqlite', check_same_thread=False)     
 cursor= conexion.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS Principal (ID_Chat PRIMARY KEY, Nombre TEXT, Usuario TEXT, Estado TEXT, V1 INT, D1 INT, V2 INT, D3 INT, V5 INT, D5 INT)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS Principal (ID_Chat PRIMARY KEY, Nombre TEXT, Usuario TEXT, Estado TEXT, V1 INT, D1 INT, V3 INT, D3 INT, V5 INT, D5 INT)''')
 cursor.execute('''CREATE TABLE IF NOT EXISTS Juego_Maquina (ID_Juego_Maquina PRIMARY KEY, ID_Persona SECONDARY KEY REFERENCES Principal(ID_Persona), Numero_Rondas INT, Ganadas INT, Perdidas INT)''')
 conexion.commit()
 
 # Inicialización de los parámetros iniciales del bot
-TOKEN = 'Token del bot'
+TOKEN = open('bot.txt','r').read() # Archivo que incluye únicamente el token del bot
 bot = telebot.TeleBot(TOKEN)
 
-
+# INICIO 
 @bot.message_handler(commands=['start'])
 def bienvenida(message):
     if Comprobar_Estado(message.chat.id)==1:
-        bot.send_message(message.chat.id, '¡Bienvenid@ al bot de Piedra, Papel, Tijeras! Puede utilizar los siguientes comandos:\n\n/partida_vs_maquina - Inicia una partida contra la máquina.\n/estadísticas - Muestra tus estadísticas generales.')
-        cursor.execute('''INSERT OR IGNORE INTO Principal (ID_Chat, Nombre, Usuario, Estado) VALUES (?, ?, ?, ?)''', (message.chat.id, message.from_user.first_name+' '+message.from_user.last_name, message.from_user.username, 'COMIENZO'))
-        
+        bot.send_message(message.chat.id, '¡Bienvenid@ al bot de Piedra, Papel, Tijeras! Puedes utilizar los siguientes comandos:\n\n/partida_vs_maquina - Inicia una partida contra la máquina.\n/estadisticas - Muestra tus estadísticas generales.')
+        cursor.execute('''INSERT OR IGNORE INTO Principal (ID_Chat, Nombre, Usuario, Estado, V1, D1, V3, D3, V5, D5) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', (message.chat.id, message.from_user.first_name+' '+message.from_user.last_name, message.from_user.username, 'INICIO', 0, 0, 0, 0, 0, 0))
+        conexion.commit()
 
+# Muestra las estadísticas del jugador
+@bot.message_handler(commands=['estadisticas'])
+def estadisticas(message):
+    if Comprobar_Estado(message.chat.id, 'INICIO'):
+        cursor.execute('''SELECT V1, D1, V3, D3, V5, D5 FROM Principal WHERE ID_Chat = ?''', (message.chat.id, ))
+        est = cursor.fetchone() # Números de las estadísticas
+        bot.send_message(message.chat.id, '''*TUS ESTADÍSTICAS*\n
+        *A ronda única* ('''+str(est[0]+est[1])+''' jugadas)
+        Victorias: '''+str(est[0])+'''
+        Derrotas: '''+str(est[0])+'''\n
+        *Al mejor de 3* ('''+str(est[2]+est[3])+''' jugadas)
+        Victorias: '''+str(est[2])+'''
+        Derrotas: '''+str(est[3])+'''\n
+        *Al mejor de 5* ('''+str(est[4]+est[5])+''' jugadas)
+        Victorias: '''+str(est[4])+'''
+        Derrotas: '''+str(est[5])+'''\n''', parse_mode='Markdown')
 
 # Funciones para sintetizar el código de la base de datos
 def Comprobar_Estado(ID_Chat, Estado=None):
--- updates.md
+++ updates.md
@@ -1,7 +1,13 @@
-# valpha.0.0
+# valpha.0.0 21/01/2022
 Añadido archivo de lectura para notificar cambios en las próximas actualizaciones.
 
-# valpha.0.1
+# valpha.0.1 22/01/2022
 Se añade la función de dar la bienvenida (sólo la primera vez) y añadir al usuario dentro de la base de datos.
 WIP -> Añadir un fichero para guardar el token del bot. Este archivo no se subirá al repositorio ya que el token debe ser privado.
 WIP -> Añadir función para comprobar estadísticas
+
+# valpha.0.2 23/01/2022
+Se añade la función de estadísticas, donde se pueden comprobar las victorias/derrotas totales jugadas desde el inicio. También se añade la lectura de un archivo para el token del bot.
+Extra: Añadidas fechas de salida de cada versión.
+WIP -> Añadir función para jugar contra la máquina: Seleccionar cuantas rondas se quieren jugar
+WIP -> Añadir función para jugar contra la máquina: Lectura de mensajes del jugador y respuesta aleatoria del bot para jugar
