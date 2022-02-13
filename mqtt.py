ab14212cac93db9e140324f1f54019de379be470
adrianmudarramartin <adrianmudarramartin@gmail.com>
2022-02-13 10:34:58 +0100

    Pequeña actualización

--- mqtt.py
+++ mqtt.py
@@ -9,10 +9,7 @@ import sqlite3
 import time
 from datetime import datetime
 import os
-
-# Prepara el entorno de trabajo fuera del repositorio
-os.system('libraries.sh')
-os.chdir('..')
+import paho.mqtt.client as mqtt
 
 # Inicialización de la base de datos
 conexion= sqlite3.connect('Datos.sqlite', check_same_thread=False)     
@@ -22,4 +19,29 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS Juego_Maquina (ID_Chat PRIMARY KEY,
 cursor.execute('''CREATE TABLE IF NOT EXISTS Juego_Amigo (ID_Chat_1 INT, ID_Chat_2 INT, Invitacion TEXT, Tipo_Partida INT, Hora_Actualizacion TEXT, V_1 INT, V_2 INT, Respuesta_1 TEXT, Respuesta_2 TEXT)''')
 conexion.commit()
 
-print('Me he ejecutado de locos')
+def on_message(client, data, msg):
+    # print(msg.topic + " " + str(msg.payload))
+    cursor.execute('''SELECT Nombre, MAX(V1A) FROM Principal''')
+    nombre = cursor.fetchone()[0]
+    client.publish('V1A',nombre)
+    cursor.execute('''SELECT Nombre, MAX(V3A) FROM Principal''')
+    nombre = cursor.fetchone()[0]
+    client.publish('V3A',nombre)
+    cursor.execute('''SELECT Nombre, MAX(V5A) FROM Principal''')
+    nombre = cursor.fetchone()[0]
+    client.publish('V5A',nombre)
+    
+client = mqtt.Client("")
+client.on_message = on_message
+client.username_pw_set(username="pi", password="patatas")
+client.connect("localhost", 1883, 60)
+client.loop_forever()
+client.subscribe("request")
+
+#try:
+#    while True:
+#        time.sleep(1)
+#except KeyboardInterrupt:
+#    print("Ok")
+#    client.disconnect()
+#    client.loop_stop()
